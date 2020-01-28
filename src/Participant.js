856ab99405f1ebe5d8e1e0b4ae39f9dfc9140ae7
Ron Martin <ronmartin89@gmail.com>
2020-01-28 12:43:55 -0800

    The sessionData is now loading the correct string

--- src/Participant.js
+++ src/Participant.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './Participant.css';
 
 function Participant(props) {
+    let sessionData = '';
+    if(props.inSession === true) {
+
+        if(props.onStage === true) {
+            sessionData = 'On Stage';
+            
+        } else if (props.onStage === false){
+            sessionData = 'In session';
+        }
+    } else if(props.inSession === false) {
+        sessionData = 'Left session';
+    }
+
     return ( 
         //JSX in here
     //<Participant key={item.id} name={item.name} avatar={item.avatar} inSession={item.inSession} onStage={item.onStage} />;
@@ -10,7 +23,9 @@ function Participant(props) {
             <h3>{props.name}</h3>
             {/* If participant is inSession the color should be green
                 if they are out of session, the color will be gray */}
-            <p></p>
+            <p>
+                {sessionData}
+            </p>
             {/* When you click the "Boot" button the client is kicked from the Session and Stage */}
             <button></button>
         </div>
